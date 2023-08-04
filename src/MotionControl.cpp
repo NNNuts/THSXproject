9556913f1e802a52173d4bc02fce5bab327baf60
NNNuts <1057814276@qq.com>
2023-08-04 08:38:30 -0400

    230804

--- src/MotionControl.cpp
+++ src/MotionControl.cpp
@@ -26,7 +26,7 @@ int PathTar = 0;
 double AGV_ERR[2] = {0, 0}; 
 // P I D maxChange maxlimit
 double PID_dir[5] = {0.3, 0, 0, 1, EIGEN_PI/4};
-double PID_spd[5] = {0.3, 0, 0, 0.5,        0.2};
+double PID_spd[5] = {0.3, 0, 0, 0.5,        0.1};
 double AGV_controlStates[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 
@@ -51,17 +51,28 @@ void init(void)
 
 void LidarOdoCallback(const nav_msgs::Odometry::ConstPtr& msg){
     // cout<<"ok"<<endl;
-    Quaterniond qua(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
-    Eigen::Vector3d eulerAngle=qua.matrix().eulerAngles(2,1,0);
+    Quaterniond qua(msg->pose.pose.orientation.w,msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
+    // Eigen::Vector3d eulerAngle=qua.matrix().eulerAngles(2,1,0);
     AGV_states[0] = msg->pose.pose.position.x;
     AGV_states[1] = msg->pose.pose.position.y;
-    AGV_states[2] = eulerAngle[0];
+    // AGV_states[2] = eulerAngle[0];
 
+    Matrix4d R,T;
+    R.block<3,3>(0,0) = qua.matrix();
+    R(3,3) = 1;
+    T = Matrix4d::Identity();
+    T(0,3) = 10;
+
+    Matrix4d RT = R*T;
+    AGV_states[2] = atan2(RT(1,3), RT(0,3));
+    
     //偏移矫正
     AGV_states[0] -= 0.260 * cos(AGV_states[2]);
     AGV_states[1] -= 0.260 * cos(AGV_states[2]);
-    // ROS_INFO("AGV当前位置为: %f, %f", msg->pose.pose.position.x, msg->pose.pose.position.y);
-
+    // ROS_INFO("AGV接收位置为: %f, %f", msg->pose.pose.position.x, msg->pose.pose.position.y);
+    ROS_INFO("AGV接收四元素为: %f, %f,%f, %f", msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
+    ROS_INFO("当前位置为: %f, %f, %f", AGV_states[0], AGV_states[1], AGV_states[2]);
+    // ROS_INFO("接受RPY为: %f, %f, %f", eulerAngle[0], eulerAngle[1], eulerAngle[2]);
 }
 
 std_msgs::Float32MultiArray CotrolCal(void)
@@ -107,8 +118,13 @@ std_msgs::Float32MultiArray CotrolCal(void)
 void CalAGVERR(void)
 {
     AGV_ERR[1] = atan2(Path[PathTar][1]-AGV_states[1],Path[PathTar][0]-AGV_states[0]);
+    AGV_ERR[1] = AGV_ERR[1] - AGV_states[2];
+    if(AGV_ERR[1] > EIGEN_PI)
+        AGV_ERR[1] -= 2*EIGEN_PI;
+    else if(AGV_ERR[1] < -EIGEN_PI)
+        AGV_ERR[1] += 2*EIGEN_PI;
     AGV_ERR[0] = sqrt((Path[PathTar][1]-AGV_states[1]*(Path[PathTar][1]-AGV_states[1]))+(Path[PathTar][0]-AGV_states[0])*(Path[PathTar][0]-AGV_states[0]));
-    AGV_ERR[0] = cos(AGV_ERR[1] - AGV_states[2]) * AGV_ERR[0];
+    AGV_ERR[0] = cos(AGV_ERR[1]) * AGV_ERR[0];
 }
 
 void AGV_ConCal(void)
@@ -164,9 +180,9 @@ int main(int argc, char **argv)
     init();
     while (ros::ok())
     {
-        ROS_INFO("当前位置为: %f, %f, %f", AGV_states[0], AGV_states[1], AGV_states[2]);
-        ROS_INFO("目标位置为: %f, %f", Path[PathTar][0], Path[PathTar][1]);
-        ROS_INFO("控制速度为: %f, 方向为：%f", AGV_control_state[0], AGV_control_state[1]);
+        // ROS_INFO("当前位置为: %f, %f, %f", AGV_states[0], AGV_states[1], AGV_states[2]);
+        // ROS_INFO("目标位置为: %f, %f", Path[PathTar][0], Path[PathTar][1]);
+        // ROS_INFO("控制速度为: %f, 方向为：%f", AGV_control_state[0], AGV_control_state[1]);
         CalAGVERR();
         AGV_ConCal();
         std_msgs::Float32MultiArray msg = CotrolCal();
